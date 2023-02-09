baeba6d7f0a16bd109972ac828eed86ddb3cc0a9
Viktor Romanov <vikrom2107@yandex.ru>
2023-02-09 21:38:36 +0700

    Change int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{10, 20, 54});
+        SalesManager salesManager = new SalesManager(new long[]{10, 20, 54});
         System.out.println("Максимальное значение: " + salesManager.max());
     }
 }
